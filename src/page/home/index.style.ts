54ea65ad4404e09272c6d2b9a3155f9a6903ff26
ziqi.wang <ziqi.wang@thoughtworks.com>
2020-08-30 15:04:31 +0800

    draw quadrant

---
+++ src/page/home/index.style.ts
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core"
+
+export const useStyles = makeStyles({
+  quadrant: {
+    width: '50vw',
+    height: '50vh',
+    border: '1px solid black',
+    borderRadius: '5px'
+  }
+})
--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,7 +1,21 @@
 import React from "react"
+import { Box } from "@material-ui/core"
+import { useStyles } from "./index.style"
 
 export type HomePageProps = {}
 const HomePage = (p: HomePageProps) => {
-    return <div>Home Page</div>
+    const styles = useStyles()
+    return (
+        <div>
+            <Box display="flex" flexDirection="row">
+                <Box className={styles.quadrant}></Box>
+                <Box className={styles.quadrant}></Box>
+            </Box>
+            <Box display="flex" flexDirection="row">
+                <Box className={styles.quadrant}></Box>
+                <Box className={styles.quadrant}></Box>
+            </Box>
+        </div>
+    )
 }
 export default HomePage
